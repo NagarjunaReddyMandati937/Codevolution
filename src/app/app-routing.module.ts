1b2eae6b10dc23b581a06c64f92355bb4fc7423d
nagarjunareddymandati937 <nagarjunamandati937@gmail.com>
2020-04-11 17:12:20 +0530

    router links added with components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { CareersComponent } from './careers/careers.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutusComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'careers', component: CareersComponent },
+  { path: 'technologies', component: TechnologyComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routeComponents = [HomeComponent,
+  AboutusComponent,
+  CareersComponent, SignupComponent,
+  LoginComponent, PageNotFoundComponent,
+  TechnologyComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routeComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    routeComponents
+    
   ],
   imports: [
     BrowserModule,
