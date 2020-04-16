d8cb6f54331a1cee8e6aa72b6a9ee69d5d954893
nagarjunareddymandati937 <nagarjunamandati937@gmail.com>
2020-04-16 11:14:01 +0530

    New Project basic model angulaCrud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fromServices';
+  title = 'Angular_Curd_With_Spring_Mysql';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    
   ],
   imports: [
     BrowserModule,
